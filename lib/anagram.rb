1391e2e70fa704c56d41214b998d1aadda040027
Shilcof <robert.shilcof@gmail.com>
2020-10-22 13:49:46 +0100

    commit tree cef26791f51556e9d9d07f40c1ef1e413036746a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select{ |el| el.downcase.chars.sort.join == @word.downcase.chars.sort.join }
+    end
+end
